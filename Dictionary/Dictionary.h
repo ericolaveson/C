229080cbe0f53ce55957c47bb2569c77cb3e07f8
ericolaveson <eric.m.olaveson@gmail.com>
2018-03-23 00:43:46 -0700

    Updating Dictionary Capabilities: Now lookup array and string keys.

--- Dictionary/Dictionary.h
+++ Dictionary/Dictionary.h
@@ -9,41 +9,58 @@
 
 
 #include <stdlib.h>
-#include <stdarg.h>
-
 
 typedef unsigned char uchar;
 typedef unsigned int  uint;
 typedef unsigned long ulong;
 
+typedef enum  {ARRAY_KEY, STRING_KEY, PRIMITIVE_KEY, NUM_KEYS} KEY_TYPE;
 
 static struct Node_t
 {
-	char  key;
-	void* data;
-
-	struct Node_t *down;
 	struct Node_t *left;
 	struct Node_t *right;
-} default_Node = {'Z', NULL, NULL, NULL, NULL};
+	struct Node_t *down;
+	char  key;
+	void* data;
+} default_Node = {NULL, NULL, NULL,'Z', NULL};
 typedef struct Node_t Node;
 
 
-static struct Dictionary_t
+typedef struct
 {
 	Node *head;
 	Node *tail;
-} default_dict = {NULL, NULL};
-typedef struct Dictionary_t Dictionary;
+	ulong key_len;
+	KEY_TYPE key_type;
+} Dictionary;
 
 
-Dictionary new_Dictionary();
-void*  insert_key(Dictionary *dict, char *key, ulong key_size, ulong data_size);
-uchar  lookup_key(Dictionary *dict, char *key, ulong key_size);
-void   destructor(Dictionary *d);
+/* CONSTRUCTORS */
+Dictionary array_dict(ulong);
+static const Dictionary string_dict  = {NULL, NULL, 0, STRING_KEY};
+static const Dictionary default_dict = {NULL, NULL, 1, PRIMITIVE_KEY};
 
-#define insert(dict, key, data_in) (*(typeof(data_in)*)insert_key(&dict, (char*)&key, sizeof(key), sizeof(data_in)) = data_in)
-#define lookup(dict, key) (lookup_key(&dict, (char*)&key, sizeof(key)))
+
+/* FUNCTIONS */
+void*  insert_datum_key (Dictionary*, char*, ulong, ulong);
+void*  insert_array_key (Dictionary*, char*, ulong, ulong);
+void*  insert_string_key(Dictionary*, char*, ulong);
+uchar  lookup_datum_key (Dictionary*, char*, ulong);
+uchar  lookup_array_key (Dictionary*, char*, ulong);
+uchar  lookup_string_key(Dictionary*, char*);
+void   destructor(Dictionary*);
+
+
+/* MACROS */
+#define insert(dict, key, data_in) (*(typeof(data_in)*)insert_datum_key(&dict, (char*)&key, sizeof(key), sizeof(data_in)) = data_in)
+#define lookup(dict, key) (lookup_datum_key(&dict, (char*)&key, sizeof(key)))
+#define insert_key(dict, key, data_in) ((dict.key_type == ARRAY_KEY) ? \
+				  (*(typeof(data_in)*)insert_array_key(&dict, (char*)key, sizeof(key[0]), sizeof(data_in)) = data_in) : \
+				  (*(typeof(data_in)*)insert_string_key(&dict, key, sizeof(data_in)) = data_in))
+#define lookup_key(dict, key) ((dict.key_type == ARRAY_KEY) ? \
+				  (lookup_array_key(&dict, (char*)key, sizeof(key[0]))) : \
+				  (lookup_string_key(&dict, key)))
 #define retreive(dict, data_out) ( data_out = *(typeof(data_out)*)dict.tail->data )
 
 
